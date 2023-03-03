26f464ef30962ac04257fce8627de0b378f20dd4
Scott Wright <scottwright.qrf@gmail.com>
2023-03-02 23:17:45 -0600

    changes

--- family_lists/settings.py
+++ family_lists/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'family_lists_app/static')
+TEMPLATE_DIRS = (os.path.join(BASE_DIR,'templates', 'static', 'staticfiles')),
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
