9f2ec8166a666743019dfeeaa9289b05230925b0
Scott Wright <scottwright.qrf@gmail.com>
2023-03-04 20:09:07 -0600

    changes

--- family_lists_app/models.py
+++ family_lists_app/models.py
@@ -2,25 +2,25 @@ from django.db import models
 
 
 class Events(models.Model):
-    item = models.CharField(max_length=30)
+    item = models.CharField(max_length=60)
 
     def __str__(self):
         return self.item
 
 class Issue(models.Model):
-    item= models.CharField(max_length=30)
+    item= models.CharField(max_length=60)
     
     def __str__(self):
         return self.item
     
 class Need(models.Model):
-    item = models.CharField(max_length=30)
+    item = models.CharField(max_length=60)
 
     def __str__(self):
         return self.item
     
 class Repair(models.Model):
-    item = models.CharField(max_length=30) 
+    item = models.CharField(max_length=60) 
     
     def __str__(self):
         return self.item
