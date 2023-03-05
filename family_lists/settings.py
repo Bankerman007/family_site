cba3a42c92c352d1e7561554e6afa87a74d7ec4c
Scott Wright <scottwright.qrf@gmail.com>
2023-03-04 19:51:56 -0600

    changes

--- family_lists/settings.py
+++ family_lists/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-sdo!k15f3ia&%2%uqw312f$$lwg407o6w4wpm_b8-1ifzrb7xh
 DEBUG=False
 #DEBUG = os.environ['DEBUG']=='TRUE'
 
-ALLOWED_HOSTS = ['wright-family-site-justdev.us','127.0.0.1']
+ALLOWED_HOSTS = ['wright-family-site.justdev.us','127.0.0.1']
 
 
 # Application definition
